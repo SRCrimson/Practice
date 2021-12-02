f9ede3976c6affcb9b235d7455c119c8372b8453
SRCrimson <92110773+SRCrimson@users.noreply.github.com>
2021-12-01 21:22:07 -0500

    Create Inventario.java

---
+++ gestorAplicacion/Loadout/Inventario.java
@@ -0,0 +1,15 @@
+package loadout;
+
+public class Inventario {
+	int n;
+	int q;
+	int z;
+	Armadura[] armaduras = new Armadura[n]; 
+	Arma[] armas = new Arma[q]; 
+	
+///////////////////////////////////////////////// or:
+	
+	Object[] objetos = new Object[z];
+	
+	
+}
