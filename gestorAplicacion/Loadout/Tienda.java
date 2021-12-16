dcd13f336113eb2b3fcdada08994b0afc19ec9fc
SRCrimson <92110773+SRCrimson@users.noreply.github.com>
2021-12-16 05:02:01 -0500

    Modificacion de precios

--- gestorAplicacion/Loadout/Tienda.java
+++ gestorAplicacion/Loadout/Tienda.java
@@ -8,37 +8,37 @@ public class Tienda {
 	
 public static List<Arma>     listaArmasGuerreroTienda  = new ArrayList<Arma>();
 public static void AñadirGuerrero() {
-	listaArmasGuerreroTienda.add(new Arma("Bisturi",   "De poco filo", 1, 100));
-	listaArmasGuerreroTienda.add(new Arma("Excalibur", "Espada legendaria", 2, 200));
-	listaArmasGuerreroTienda.add(new Arma("Ragnarok",  "World ender", 5, 500));
+	listaArmasGuerreroTienda.add(new Arma("Bisturi",   "De poco filo", 5, 25));
+	listaArmasGuerreroTienda.add(new Arma("Excalibur", "Espada legendaria", 10, 50));
+	listaArmasGuerreroTienda.add(new Arma("Ragnarok",  "World ender", 15, 100));
 }
 
 public static List<Arma>     listaArmasArqueroTienda   = new ArrayList<Arma>();
 public static void AñadirArquero() {
-	listaArmasArqueroTienda.add(new Arma("Arco de Madera",   "Para alguien habil no resultara dificil usarlo", 1, 100));       
-	listaArmasArqueroTienda.add(new Arma("Arco Compuesto",   "Destaca por su precision y dureza", 2, 200));
-	listaArmasArqueroTienda.add(new Arma("Azkar",  		     "Solo es posible usarlo en manos del el mas habil arquero", 5, 500));
+	listaArmasArqueroTienda.add(new Arma("Arco de Madera",   "Para alguien habil no resultara dificil usarlo", 5, 25));       
+	listaArmasArqueroTienda.add(new Arma("Arco Compuesto",   "Destaca por su precision y dureza", 10, 50));
+	listaArmasArqueroTienda.add(new Arma("Azkar",  		     "Solo es posible usarlo en manos del el mas habil arquero", 15, 100));
 }
 
 public static List<Arma>     listaArmasMagoTienda      = new ArrayList<Arma>();
 public static void AñadirMago() {
-	listaArmasMagoTienda.add(new Arma("Baston de runas",   "Arma preferida por jovenes aprendices", 1, 100));       
-	listaArmasMagoTienda.add(new Arma("Baston astral ",    "Pocos han podido emplearlo a su maximo potencial", 2, 200));
-	listaArmasMagoTienda.add(new Arma("Omnirod",           "Pasado de generacion en generacion, con poderes ocultos latentes", 5, 500));
+	listaArmasMagoTienda.add(new Arma("Baston de runas",   "Arma preferida por jovenes aprendices", 5, 25));       
+	listaArmasMagoTienda.add(new Arma("Baston astral ",    "Pocos han podido emplearlo a su maximo potencial", 10, 50));
+	listaArmasMagoTienda.add(new Arma("Omnirod",           "Pasado de generacion en generacion, con poderes ocultos latentes", 15, 100));
 }
 
 public static List<Armadura> listaArmadurasTienda      = new ArrayList<Armadura>();
 public static void AñadirArmaduras() {
-	listaArmadurasTienda.add(new Armadura("Armadura de bronce", "De poco costo y dureza respetable", 1, 100));
-	listaArmadurasTienda.add(new Armadura("Armadura de plata",  "Otorgada a distinguidos heroes por sus haza�as", 2, 200));
-	listaArmadurasTienda.add(new Armadura("Armadura de Oro",    "Solo puede ser forjada una vez cada 100 a�os", 3, 300));
+	listaArmadurasTienda.add(new Armadura("Armadura de bronce", "De poco costo y dureza respetable", 5, 10));
+	listaArmadurasTienda.add(new Armadura("Armadura de plata",  "Otorgada a distinguidos heroes por sus haza�as", 10, 20));
+	listaArmadurasTienda.add(new Armadura("Armadura de Oro",    "Solo puede ser forjada una vez cada 100 a�os", 20, 40));
 }
 
 public static List<Pocion>   listaPocionesTienda         = new ArrayList<Pocion>();
 public static void AñadirPociones() {
-	listaPocionesTienda.add(new Pocion("Pocion Small",  "Otorga 10 de vida al jugador", 20, 100));
-	listaPocionesTienda.add(new Pocion("Pocion Medium", "Otorga 25 de vida al jugador", 25, 200));
-	listaPocionesTienda.add(new Pocion("Pocion Large",  "Otorga la mitad de la vida al jugador", 50, 300));
+	listaPocionesTienda.add(new Pocion("Pocion Small",  "Otorga 10 de vida al jugador", 10, 10));
+	listaPocionesTienda.add(new Pocion("Pocion Medium", "Otorga 25 de vida al jugador", 25, 20));
+	listaPocionesTienda.add(new Pocion("Pocion Large",  "Otorga la mitad de la vida al jugador", 50, 30));
 }
 
 public static void ComprarArmaGuerrero(char nombre, int wallet) {
